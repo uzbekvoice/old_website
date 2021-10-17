560b25a66d3afaba4ad537a7f5d04a1d102c3cbd
Ruslan Yuldashev <monitoringe@gmail.com>
2021-10-18 01:49:18 +0500

    Leaderboard API

--- script/common.js
+++ script/common.js
@@ -93,3 +93,29 @@ $('.partners__catalog').slick({
   
   ]
 });
+
+
+$(document).ready(function(){
+  function loadLeaderboard(listEl, url) {
+    $.get(url).done(function(data){
+      let block = listEl.find('.leader__wall-info');
+      listEl.find('.ajax-loader').hide();
+      data.forEach(function(item, i) {
+        if (i >= 3) return;
+        let newBlock = block.clone();
+        listEl.append(newBlock);
+        newBlock.find('.leaderboard-username').text(item.username);
+        newBlock.find('.leaderboard-total').text(item.total);
+        newBlock.show();
+      });
+    })
+  }
+
+  const leaderboardClips = $('.leaderboard-list-clips');
+  const leaderboardClipsURL = 'http://104.248.26.69/leaderboard/clips';
+  loadLeaderboard(leaderboardClips, leaderboardClipsURL);
+
+  const leaderboardVotes = $('.leaderboard-list-votes');
+  const leaderboardVotesURL = 'http://104.248.26.69/leaderboard/votes';
+  loadLeaderboard(leaderboardVotes, leaderboardVotesURL);
+});
